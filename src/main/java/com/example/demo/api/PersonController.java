1fcfd6774df2d5eb7c94dc260e7dab176de49a72
Marguerite Blair <marguerite.blair@gmail.com>
2020-11-08 15:14:51 -0500

    added spring flags to the file

--- src/main/java/com/example/demo/api/PersonController.java
+++ src/main/java/com/example/demo/api/PersonController.java
@@ -2,15 +2,24 @@ package com.example.demo.api;
 
 import com.example.demo.model.Person;
 import com.example.demo.service.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+
+@RestController
 public class PersonController {
 
     private final PersonService personService;
 
+    @Autowired
     public PersonController(PersonService personService) {
         this.personService = personService;
     }
 
+    //this method will be a post request
+    //we need to tell spring this will be used as a psot
+    @PostMapping
     public void addPerson(Person person) {
         personService.addPerson(person);
     }
--- src/main/java/com/example/demo/dao/FakePersonDataAccessService.java
+++ src/main/java/com/example/demo/dao/FakePersonDataAccessService.java
@@ -1,11 +1,14 @@
 package com.example.demo.dao;
 
 import com.example.demo.model.Person;
+import jdk.jfr.Registered;
+import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+@Repository("fakeDao") //can also do @Repository
 public class FakePersonDataAccessService implements PersonDao{
     private static List<Person> DB = new ArrayList<>();
 
