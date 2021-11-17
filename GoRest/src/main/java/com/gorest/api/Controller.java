0924f0776e14adaa9274eccc60ee33b561170443
Marguerite Blair <marguerite.blair@gmail.com>
2021-11-17 16:41:21 -0500

    so many rabbit holes

--- GoRest/src/main/java/com/gorest/api/Controller.java
+++ GoRest/src/main/java/com/gorest/api/Controller.java
@@ -4,8 +4,15 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.gorest.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 
 @RequestMapping("/public/v1")
@@ -20,7 +27,24 @@ public class Controller {
 
     @GetMapping(value = "/users/{id}")
     public String getUserById(@PathVariable("id") String id) throws JsonProcessingException {
-        return restTemplate.getForObject("https://gorest.co.in/public/v1/users/{id}", String.class);
+        String url = "https://gorest.co.in/public/v1/users/{id}";
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+
+        HttpEntity request = new HttpEntity(headers);
+        ResponseEntity<String> response = restTemplate.exchange(
+                url,
+                HttpMethod.GET,
+                request,
+                String.class,
+                id
+        );
+        if (response.getStatusCode() == HttpStatus.OK) {
+            return "worked";
+        } else {
+            return "req failed bitch";
+        }
     }
 
     @PostMapping(value = "users/", consumes = "application/json", produces = "application/json")
