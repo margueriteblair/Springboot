0b63b6afaa5f5f8b168a3787704ccbc45a047a2a
Marguerite Blair <marguerite.blair@gmail.com>
2021-11-17 15:01:35 -0500

    setting up controller

---
+++ GoRest/src/main/java/com/gorest/api/Controller.java
@@ -0,0 +1,9 @@
+package com.gorest.api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("api/v1/")
+@RestController
+public class Controller {
+}
