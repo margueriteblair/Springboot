c6600edf0ed6a08d63b9c54cfb07902193196fcf
Marguerite Blair <marguerite.blair@gmail.com>
2021-11-17 16:02:53 -0500

    incorporating rest templates

--- GoRest/src/main/java/com/gorest/api/Controller.java
+++ GoRest/src/main/java/com/gorest/api/Controller.java
@@ -1,28 +1,32 @@
 package com.gorest.api;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.gorest.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 
-
-@RequestMapping("/https://goest.co.in/public/v1/")
+@RequestMapping("/public/v1")
 @RestController
 public class Controller {
 
     @Autowired
     Environment env;
 
+    @Autowired
+    RestTemplate restTemplate;
+
     @GetMapping(value = "/users/{id}")
-    public String getUserById(@PathVariable("id") String id) {
-        return "Hey, test worked for " + id;
+    public String getUserById(@PathVariable("id") String id) throws JsonProcessingException {
+        return restTemplate.getForObject("https://gorest.co.in/public/v1/users/{id}", String.class);
     }
 
     @PostMapping("users/")
     public User createUser(@RequestBody User user){
         if (user == null) {
-            throw new IllegalArgumentException("User must not be null & have all required fields")
+            throw new IllegalArgumentException("User must not be null & have all required fields");
         } else {
             return user;
         }
