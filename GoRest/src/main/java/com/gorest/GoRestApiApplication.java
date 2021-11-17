1d6554cf97e9b8e0afa9923a40357787a9ba3dc4
Marguerite Blair <marguerite.blair@gmail.com>
2021-11-17 14:55:17 -0500

    go rest api set up

---
+++ GoRest/src/main/java/com/gorest/GoRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.gorest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoRestApiApplication.class, args);
+	}
+
+}
---
+++ GoRest/src/test/java/com/gorest/GoRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gorest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
