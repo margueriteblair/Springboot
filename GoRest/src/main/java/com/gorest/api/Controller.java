1649b12f0019fdabac48ed67fe6f6fe15b2e2425
Marguerite Blair <marguerite.blair@gmail.com>
2021-11-17 15:30:45 -0500

    fleshing out controler

--- GoRest/src/main/java/com/gorest/api/Controller.java
+++ GoRest/src/main/java/com/gorest/api/Controller.java
@@ -1,9 +1,22 @@
 package com.gorest.api;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-@RequestMapping("api/v1/")
+@RequestMapping("/https://goest.co.in/public/v1/")
 @RestController
 public class Controller {
+
+    @GetMapping(value = "/users/{id}")
+    public User getUserById(@PathVariable("id") String id) {
+//        return https://gorest.co.in/public/v1/users/{id}
+    }
+
+    @PostMapping(path = "users/")
+    public User createUser(@RequestBody User user){
+        if (user == null) {
+            throw new ServerException();
+        } else {
+            return new ResponseEntity<>(user, HttpStatus.CREATED);
+        }
+    }
 }
