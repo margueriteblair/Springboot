9cb5a60fc847c9243c52c30dfadaabae499dff9f
Marguerite Blair <marguerite.blair@gmail.com>
2021-11-18 17:02:58 -0500

    setting default port number

--- GoRest/src/main/java/com/gorest/GoRestApiApplication.java
+++ GoRest/src/main/java/com/gorest/GoRestApiApplication.java
@@ -2,10 +2,15 @@ package com.gorest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class GoRestApiApplication {
 
+	public RestTemplate restTemplate (RestTemplateBuilder builder) {
+		return builder.build();
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(GoRestApiApplication.class, args);
 	}
