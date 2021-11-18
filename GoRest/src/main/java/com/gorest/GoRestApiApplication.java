5a31c1e26cb38bb8ad99481440b1a9b5c3b4274b
Marguerite Blair <marguerite.blair@gmail.com>
2021-11-18 17:06:21 -0500

    bean annotation

--- GoRest/src/main/java/com/gorest/GoRestApiApplication.java
+++ GoRest/src/main/java/com/gorest/GoRestApiApplication.java
@@ -3,11 +3,13 @@ package com.gorest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class GoRestApiApplication {
 
+	@Bean
 	public RestTemplate restTemplate (RestTemplateBuilder builder) {
 		return builder.build();
 	}
